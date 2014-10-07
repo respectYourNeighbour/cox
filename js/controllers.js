98b1bc39333d2dd32a43c04af1d0d38f189da0be
respectYourNeighbour <philip_luca@yahoo.com>
2014-10-07 17:04:34 +0300

    added search options

--- js/controllers.js
+++ js/controllers.js
@@ -7,11 +7,15 @@ var myAngularApp = angular.module('myAngularApp', []);
 myAngularApp.controller('myListCtrl', function($scope) {
   $scope.heroes = [
     {'name': 'Spider-Man',
-     'snippet': 'Fast and it has the ability to create spider-webs.'},
+     'snippet': 'Fast and it has the ability to create spider-webs.',
+     'age':'21'},
     {'name': 'Storm',
-     'snippet': 'Has the ability to start weather storms.'},
+     'snippet': 'Has the ability to start weather storms.',
+ 	 'age':'32'},
     {'name': 'Profesor-X™',
-     'snippet': 'Super chill cool guy.'}
+     'snippet': 'Super chill cool guy.',
+     'age':'43'}
   ];
   $scope.name = 'user';
+  $scope.orderProperty = 'age';
 });
