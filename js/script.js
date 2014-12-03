5ca22b19238fb368536531fdbe3eb33bb24079ac
respectYourNeighbour <philip_luca@yahoo.com>
2014-12-03 14:23:24 +0200

    added normalize.css CDN
    normalize.css CDN

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
     // use jquery
-    function saySomething(){
-    	console.log('Hello World! ')
-    }
-   /* $('#title').css('color','green');*/
+  	console.log("Document ready!");
 });
