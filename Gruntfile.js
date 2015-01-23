cd52e13a6a0342214b1f9ad905bfa7c52d00ce55
respectYourNeighbour <philip_luca@yahoo.com>
2015-01-23 12:28:20 +0200

    fixed task grunt-json-merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,103 +1,98 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    
-    htmlmin: {
-      dest:{
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        htmlmin: {
+            dest:{
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dest/index.html':'*.html'
+                }
+            }
         },
-        files: {
-          'dest/index.html':'*.html'
-        }
-      }
-    },
-    less: {
-      development: {
-        options: {
-          paths: ["css/*.css"]
+        less: {
+            development: {
+                options: {
+                    paths: ["css/*.css"]
+                },
+                files: {
+                    "css/style.css": "css/*.less"
+                }
+            }
         },
-        files: {
-          "css/style.css": "css/*.less"
-        }
-      }
-    },
-    concat: {
-      // options: {
-      //   separator: ';'
-      // },
-      js: {
-        src: 'js/**/*.js', 
-        dest: 'dest/deletable/<%= pkg.name %>.js'
-      },
-      css: {
-        src: 'css/*.css',
-        dest: 'dest/deletable/<%= pkg.name %>.css'
-      }
-    }, 
-    cssmin: {
-      add_banner: {
-        options: {
-          banner: '/* My minified css file */'
+        concat: {
+            // options: {
+            //   separator: ';'
+            // },
+            js: {
+                src: 'js/**/*.js', 
+                dest: 'dest/deletable/<%= pkg.name %>.js'
+            },
+            css: {
+                src: 'css/*.css',
+                dest: 'dest/deletable/<%= pkg.name %>.css'
+            }
+        }, 
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '/* My minified css file */'
+                },
+                files: {
+                    'dest/css/style.min.css': ['<%= concat.css.dest %>']
+                }
+            }
         },
-        files: {
-          'dest/css/style.min.css': ['<%= concat.css.dest %>']
-        }
-      }
-    },
-    jsonmin: {
-      stripAll: {
-        options: {
-          stripWhitespace: true,
-          stripComments: true
+        "merge-json": {
+            "en": {
+                src: [ "resources/*.json" ],
+                dest: "dest/resources/lista.json"
+            }
         },
-        files: {
-          'dest/resources/file1.json': ['resources/file1.json'],
-          'dest/resources/file2.json': ['resources/file2.json'],
-          'dest/resources/file3.json': ['resources/file3.json'],
-          'dest/resources/file4.json': ['resources/file4.json'],
-          'dest/resources/file5.json': ['resources/file5.json'],
-          'dest/resources/lista.json': ['resources/lista.json']
+        jsonmin: {
+            stripAll: {
+                options: {
+                    stripWhitespace: true,
+                    stripComments: true
+                },
+                files: {
+                    'dest/resources/lista1.json': ['dest/resources/*.json']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dest: {
+                files: {
+                    'dest/js/app.min.js': ['<%= concat.js.dest %>']
+                }
+            }
+        },
+        watch: {
+            files: ['css/**/*.less', 'css/**/*.css','js/**/*.js','*.html', 'resources/*.json'],
+            tasks: ['newer:less', 'newer:concat', 'newer:cssmin','newer:uglify', 'newer:htmlmin', 'newer:merge-json', 'newer:jsonmin'],
+            options: {livereload: true}
         }
-      }
-    },
-    "merge-json": {
-        "en": {
-            src: [ "resources/*.json" ],
-            dest: "dest/resources/lista1.json"
-        }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dest: {
-        files: {
-          'dest/js/app.min.js': ['<%= concat.js.dest %>']
-        }
-      }
-    },
-    watch: {
-      files: ['css/**/*.less', 'css/**/*.css','js/**/*.js','*.html', 'resources/*.json'],
-      tasks: ['newer:less', 'newer:concat', 'newer:cssmin','newer:uglify', 'newer:htmlmin', 'newer:merge-json', 'newer:jsonmin'],
-      options: {livereload: true}
-    }
-  });
-  
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-merge-json');
-  grunt.loadNpmTasks('grunt-jsonmin');
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  
-  grunt.registerTask('default', ['newer:less', 'newer:concat', 'newer:cssmin', 'newer:uglify','newer:htmlmin', 'newer:merge-json', 'newer:jsonmin']);
-  grunt.registerTask('watchme', ['watch']);
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-merge-json');
+    grunt.loadNpmTasks('grunt-jsonmin');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['newer:less', 'newer:concat', 'newer:cssmin', 'newer:uglify','newer:htmlmin', 'newer:merge-json', 'newer:jsonmin']);
+    grunt.registerTask('watchme', ['watch']);
 };
 
 /*
