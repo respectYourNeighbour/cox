70d609092c81373b32d95c9d0955fe63035c7720
respectYourNeighbour <philip_luca@yahoo.com>
2015-01-28 15:33:18 +0200

    added comments to the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,8 +110,16 @@ module.exports = function(grunt) {
 };
 
 /*
-Concat: Cauta toate fisierele din folderul src cu extensia .js si le concateneaza.
-Uglify: Banerul este un comentariu ce va fi pus in fisierul minificat. Uglify va face minify doar la fisierele .js
-CSSMIN: Va face minify la fisierele de CSS.
-WATCH: Va monitoriza fisierele trecute in parametrul din watch si va efectua anumite taskuri atunci cand unul din fisiere este modificat.
+1. "newer:less"       -> Converting LESS file to CSS in folder /css;
+2. "newer:concat"     -> Concatenating the JS and CSS files to only 1 file each, from folder /js and /css, and sending them to folder dest/deletable;
+3. "newer:htmlmin"    -> Minifying the .html files and send them to folder /dest;
+4. "newer:uglify"     -> Minifying the JS files from folder dest/deletable to folder dest/js;
+5. "newer:merge-json" -> Merging JSON files to only 1 JSON file to folder /dest/resources/list.json;
+6. "newer:jsonmin"    -> Minifying the JSON file;
+7. "uncss"            -> Cleaning the CSS file from folder /dest/deletable/*.css to another file in the same folder tidy.css;
+8. "cssmin"           -> Minifying the tidy CSS file; 
+
+WATCH: will monitor for changes in all the files mentioned in the watch task, and will execute some task when changes were made to the monitored files;
+NEWER: will only trigger a grunt task if the source file is newer than the destination files;
+
 */
