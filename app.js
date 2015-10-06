b04ee149600e15d7038b9e0c1754c01107f8fefc
respectYourNeighbour <philip_luca@yahoo.com>
2015-10-06 21:31:01 +0300

    push test owner pot sa scriu ce vreau asta e mesajul de commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	config = require('./config'),
 	port = 3000;
-
+	// linie de test
 
 	mongoose.connect('mongodb://127.0.0.1/fridge');
     var db = mongoose.connection;
