e7c6274b8ec29ee9893e2c84e94f484a0198d6bd
respectYourNeighbour <philip_luca@yahoo.com>
2015-10-10 13:21:53 +0300

    changes made in css and index.html for the recepy

--- public/js/controllers/NavbarCtrl.js
+++ public/js/controllers/NavbarCtrl.js
@@ -29,4 +29,5 @@ angular.module('main_app').controller('NavbarCtrl', function($scope, $state, Log
         //console.log("NavbarCtrl isAuthenticated",LoginService.isAuthenticated())
         return LoginService.isAuthenticated();
     };
+
 });
