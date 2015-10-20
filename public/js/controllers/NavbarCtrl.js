1185e1ce97260599c3f4ef3406b94c4733ed7c75
Andrei Luca <andrei.luca@windriver.com>
2015-10-20 11:56:54 +0300

    modifications to menu

--- public/js/controllers/NavbarCtrl.js
+++ public/js/controllers/NavbarCtrl.js
@@ -10,17 +10,42 @@ angular.module('main_app').controller('NavbarCtrl', function($scope, $state, Log
         });
     }
 
-    showLeft.onclick = function() {
+    /*showLeft.onclick = function() {
         $(this).toggleClass("active");
         $(this).toggleClass("effect1");
         $("body").toggleClass("show_sidebar");
-    };
+    };*/
 
     /*$("#showLeftPush").on("click", function() {
         console.log(">>>>>>>>>>>>>>>click")
         $('body').toggleClass("cbp-spmenu-push-toright");
         $("#cbp-spmenu-s1").toggleClass("cbp-spmenu-open")
     });*/
+
+$("#menu-toggle").click(function(e) {
+  console.log("click 1")
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        //$("#showLeft").toggleClass("effect1");
+    });
+
+/*
+$("#showLeft").click(function(e) {
+  console.log("click 1")
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+         $(this).toggleClass("active");
+        $(this).toggleClass("effect1");
+    });
+*/
+
+     $("#menu-toggle-2").click(function(e) {
+      console.log("click 2")
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-2");
+        $('#menu ul').hide();
+    });
+
 
     $(".cbp-spmenu a").on("click", function() {
         console.log(">>>>>>>>>>>>>>>click")
---
+++ public/js/my.js
@@ -0,0 +1,25 @@
+    $(document).ready(function() {initMenu();});
+
+
+
+
+     function initMenu() {
+      console.log("initmenu")
+      $('#menu ul').hide();
+      $('#menu ul').children('.current').parent().show();
+      //$('#menu ul:first').show();
+      $('#menu li a').click(
+        function() {
+          console.log("menu li a click")
+          var checkElement = $(this).next();
+          if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+            return false;
+            }
+          if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+            $('#menu ul:visible').slideUp('normal');
+            checkElement.slideDown('normal');
+            return false;
+            }
+          }
+        );
+      }
